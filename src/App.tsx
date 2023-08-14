6567d980956705a418c8686d1c9ab8c2faba4f26
Maia-Mateus <matteusmaia889@gmail.com>
2023-08-13 23:39:29 -0300

    HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import logoImg from './assets/logo.png'
 import './App.css'
 
 export function App() {
@@ -7,7 +7,24 @@ export function App() {
 
   return (
     <>
+      <div className="container">
+        <img 
+          src={logoImg}
+          alt="logo frases" 
+          className='logo'
+        />
 
+        <h2 className='title'>Categorias</h2>
+
+        <section className='category-area'>
+          <button className="category-button">Motivação</button>
+          <button className="category-button">Bem estar</button>
+        </section>
+
+        <button className='button-frase'>Gerar frase</button>
+
+        <h3 className="texto-frase">Alguma frase vai vir aqui</h3>
+      </div>
     </>
   )
 }
