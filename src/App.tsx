ff0374461d7978c5a6bc00c2ea2786dc76d4a8fc
Maia-Mateus <matteusmaia889@gmail.com>
2023-08-16 19:37:24 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-
+import { useState } from 'react'
 
 import logoImg from './assets/logo.png'
 import './App.css'
 
 export function App() {
 
-  
+  const [textoFrase, setTextoFrase] = useState('')
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState(0)
+
+  const allFrases = [
+    {
+      id: 1,
+      nome: 'Motivação',
+      frases: [
+        'Siga os bons e aprenda com eles.',
+        'O bom-senso vale mais do que muito conhecimento.',
+        'O riso é a menor distância entre duas pessoas.',
+        'Deixe de lado as preocupações e seja feliz.',
+        'Realize o óbvio, pense no improvável e conquiste o impossivel.',
+        'Acredite em milagres, mas não dependa deles.',
+        'A maior barreira para o sucesso é o medo do fracasso.'
+      ]
+    },
+    {
+      id: 2,
+      nome: 'Bom dia',
+      frases: [
+        'Acordar de bem com a vida é o primeiro passo para ter um dia abençoado! Bom dia, família!',
+        "A melhor forma de acordar é levantar da cama e se preparar para correr atrás de todos os seus sonhos! Bom dia, mundo!",
+        "Escreva em seu coração: todo dia é o melhor dia do ano!",
+        "Bom dia! Não se esqueça que sua alma é o reflexo do sol, tão forte e brilhante quanto um girassol."
+      ]
+    }
+  ]
 
   return (
     <>
@@ -19,13 +46,28 @@ export function App() {
         <h2 className='title'>Categorias</h2>
 
         <section className='category-area'>
-          <button className="category-button">Motivação</button>
-          <button className="category-button">Bem estar</button>
+          
+          {allFrases.map((item) => (
+            <button 
+              key={item.id}
+              className="category-button"
+              style={{ 
+                borderWidth: item.nome === allFrases[categoriaSelecionada].nome ? 2 :0
+              }}
+            >
+              {item.nome}
+            </button>
+          ))}
+          
         </section>
 
         <button className='button-frase'>Gerar frase</button>
 
-        <p className="texto-frase">Alguma frase vai vir aqui</p>
+        {textoFrase !== '' && (
+          <p className="texto-frase">
+            {textoFrase}
+          </p>
+        )}
       </div>
     </>
   )
